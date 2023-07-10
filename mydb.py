0d8d8c92cb458fd4aa8991429327d928cf6ad4f4
george-apps <raptis621@gmail.com>
2023-07-11 00:08:21 +0300

    registration

--- mydb.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector
-
-dataBase= mysql.connector.connect(host='localhost',user='root',passwd='pass123')
-
-cursorObj=dataBase.cursor()
-
-cursorObj.execute("CREATE DATABASE forfun")
